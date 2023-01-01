d56c75450909809011ef6581732ad61c4928175f
Ravi Gamage <ravindugamage7@gmail.com>
2023-01-01 12:45:08 +1100

    websocket connection error

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "multiplayer-snake-ravig31.netlify.app",
+    origins: "*:*",
     methods: ["GET", "POST"],
     allowedHeaders: ["*"],
     credentials: true
